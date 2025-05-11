a0a15d2a18602ae14cb306caccf2cb4f2202f362
Atakan Bal <atakannbal@gmail.com>
2025-05-12 01:13:13 +0300

    feat: use bastion ami

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,6 +10,8 @@ output "backend_ecr_repository_url" {
 
 output "name_servers" {
   description = "Name servers for the Route53 hosted zone"
-  value = length(module.ext-dns) > 0 ? module.ext-dns[0].route53_name_servers : []
+output "bastion_ssh_command" {
+  description = "Command to SSH into the bastion host"
+  value       = local.bastion_exists ? module.bastion[0].bastion_ssh_command : "No bastion host available"  
 }
 
